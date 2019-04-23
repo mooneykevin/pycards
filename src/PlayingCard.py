16d2ca44a5a07906f660dbfb944674bb388264c3
Kevin Mooney <9126997+mooneykevin@users.noreply.github.com>
2019-04-23 18:26:47 -0500

    refactored to use dicts instead of lists

--- src/PlayingCard.py
+++ src/PlayingCard.py
@@ -11,21 +11,21 @@ Possibilities:
     - a Hand class that provides dynamic user management of some number of Cards.
 
 '''
-import random
+
 
 class PlayingCard:
 
-    suits = ('C', 'H', 'D', 'S')
-    ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+    suits = {'C': "Clubs", 'H':"Hearts", 'D':"Diamonds", 'S':"Spades"}
+    ranks = {1:'2', 2:'3', 3:'4', 4:'5', 5:'6', 6:'7', 7:'8', 8:'9', 9:'10', 10:'J', 11:'Q', 12:'K', 13:'A'}
 
-    suit = ''
-    rank = ''
+    suit = None
+    rank = None
 
     # default to standard 52 playing card deck
-    def __init__(self, suit=0, rank=0):
-        self.suit = self.suits[suit]
+    def __init__(self, suit='C', rank=1):
+        self.suit = suit
         self.rank = self.ranks[rank]
 
 
     def __str__(self):
-        return '[' + '%2s ' % self.rank + self.suit + ']'
+        return '[' + '%2s ' % self.rank + ' of ' + self.suits[self.suit] + ']'
