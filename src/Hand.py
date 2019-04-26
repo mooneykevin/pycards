1a18ddb6b1c353a4ce2192e42e6e09a604feca5b
Kevin Mooney <9126997+mooneykevin@users.noreply.github.com>
2019-04-25 21:03:16 -0500

    impl add, remove, contains, display

--- src/Hand.py
+++ src/Hand.py
@@ -1,4 +1,29 @@
-import PlayingCard
-
 class Hand:
-    pass
+
+    hand = []
+
+    def __init__(self):
+        self.hand = []
+
+    def add(self, card):
+        self.hand.append(card)
+
+    # this is O(n^2) but we'll take it for now since n < 53
+    def remove(self, card):
+        for c in self.hand:
+            if str(c) == str(card):
+                self.hand = [i for i in self.hand if not(i == card)]
+
+    # works because our PlayingCard class has a __str__ method defined
+    def display(self):
+        for card in self.hand:
+            print(str(card))
+
+    def contains(self, card):
+        return self.__contains__(card)
+
+    def __contains__(self, item):
+        for card in self.hand:
+            if item.suit == card.suit and item.rank == card.rank:
+                return True
+        return False
