eb420675f903cacbdb777788c98ea2996c0c4012
jalmouslli <jalmouslli@users.noreply.github.com>
2019-05-02 18:33:19 -0500

    renamed some functions; removed docstring

--- src/Deck.py
+++ src/Deck.py
@@ -1,34 +1,24 @@
 # Deck.py
 # Authors: Jamal Almouslli, Kevin Mooney
-'''
-
-Needs:
-    - a Card class with enumerable suits (faces) and ranks (values)
-    - a Deck class with standard implementations of card deck behaviors, such as
-        shuffling, dealing, and returning basic information such as the size of the deck.
-
-Possibilities:
-    - a Hand class that provides dynamic user management of some number of Cards.
-
-'''
 
 import PlayingCard
 import random
+
 
 class Deck:
 
     cards = []
     card_type = ''
 
-    def __init__(self, card_type='PlayingCard'):
+    def __init__(self):
         self.cards = []
         for suit in ['C', 'H', 'D', 'S']:
-            for rank in range(1,14):
+            for rank in range(1, 14):
                 card = PlayingCard.PlayingCard(suit, rank)
                 self.cards.append(card)
 
     def deal(self):
-        if self.isNotEmpty():
+        if self.empty():
             return self.cards.pop()
         else:
             return None
@@ -37,7 +27,7 @@ class Deck:
         for card in self.cards:
             print(str(card))
 
-    def isNotEmpty(self):
+    def empty(self):
         return len(self.cards) > 0
 
     def shuffle(self):
