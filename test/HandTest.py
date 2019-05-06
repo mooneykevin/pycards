ba1d640afc044391cda46462dce813d35e0e753e
jalmouslli <jalmouslli@users.noreply.github.com>
2019-05-05 20:02:17 -0500

    added new tests

--- test/HandTest.py
+++ test/HandTest.py
@@ -23,21 +23,26 @@ class HandTest:
     hand5 = Hand()
     hand5.add(deck.deal(5))
 
-    # should print nothing
+    # should print no cards
     print("\nfirst hand:\n")
+    print("empty: " + str(hand.empty()))
     print(hand)
 
-    # should also print nothing
+    # should also print no cards
     print("\nsecond hand:\n")
+    print("empty: " + str(hand2.empty()))
     print(hand2)
 
     # should just print 1 card
     print("\nthird hand:\n")
+    print("empty: " + str(hand3.empty()))
     print(hand3)
 
     # should also just print 1 card
     print("\nfourth hand:\n")
+    print("empty: " + str(hand4.empty()))
     print(hand4)
 
     print("\nfifth hand:\n")
+    print("empty: " + str(hand5.empty()))
     print(hand5)
