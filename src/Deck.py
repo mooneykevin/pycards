f17e013e060c6edbf18952d22b0eba460aea07fd
Kevin Mooney <9126997+mooneykevin@users.noreply.github.com>
2019-06-23 17:26:04 -0500

    renamed cards[] to deck[]

--- src/Deck.py
+++ src/Deck.py
@@ -7,38 +7,39 @@ import random
 
 class Deck:
 
-    cards = []
+    deck = []
     card_type = ''
 
     def __init__(self):
-        self.cards = []
+        self.deck = []
         for suit in ['C', 'H', 'D', 'S']:
             for rank in range(1, 14):
                 card = PlayingCard.PlayingCard(suit, rank)
-                self.cards.append(card)
+                self.deck.append(card)
+        self.shuffle()
 
     def deal(self, num_cards=1):
         cards_dealt = []
-        if len(self.cards)>= num_cards:
+        if len(self.deck) >= num_cards:
             for i in range(0, num_cards):
-                cards_dealt.append(self.cards.pop())
+                cards_dealt.append(self.deck.pop())
             return cards_dealt
         else:
             return None
 
 
     def print(self):
-        for card in self.cards:
+        for card in self.deck:
             print(str(card))
 
     def empty(self):
-        return len(self.cards) == 0
+        return len(self.deck) == 0
 
     def shuffle(self):
-        random.shuffle(self.cards)
+        random.shuffle(self.deck)
 
     def reset(self):
         self.__init__()
 
     def __len__(self):
-        return len(self.cards)
+        return len(self.deck)
