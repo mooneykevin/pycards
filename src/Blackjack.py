ddf8fecc3e3bd970b1117ca09bcf610e22e0a8c5
Kevin Mooney <9126997+mooneykevin@users.noreply.github.com>
2019-06-23 17:25:22 -0500

    spaghetti code

--- src/Blackjack.py
+++ src/Blackjack.py
@@ -1,7 +1,11 @@
 from Player import Player
+from Dealer import Dealer
 from Hand import Hand
 from Deck import Deck
 
+player = Player("Kevin", 1000)
+dealer = Dealer("House", 1000000)
+deck = Deck()
 
-class Blackjack:
-    pass
+player.draw(deck.deal(2))
+player.show_hand()
