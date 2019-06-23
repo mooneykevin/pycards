4c02875244162b4cf14c6f470a0ab434f6f1904a
Kevin Mooney <9126997+mooneykevin@users.noreply.github.com>
2019-06-23 17:26:21 -0500

    subclassed Player and added __init__

--- src/Dealer.py
+++ src/Dealer.py
@@ -1,7 +1,13 @@
 import Deck
+import Player
 
-class Dealer:
+class Dealer(Player):
+
     deck = Deck.Deck()
+
+    def __init__(self):
+        self.name = "House"
+        self.money = 1000000
 
     def deal(self, num_cards=1):
         cards = []
