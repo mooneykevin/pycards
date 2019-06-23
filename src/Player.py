2a3dadc3783a28746b9510f1bb0bd95a6c7c2d97
Kevin Mooney <9126997+mooneykevin@users.noreply.github.com>
2019-06-23 17:26:50 -0500

    improvements

--- src/Player.py
+++ src/Player.py
@@ -1,19 +1,24 @@
+from Hand import Hand
+
 class Player:
 
     name = None
     _hand = None
     money = 0
 
-    def __init__(self, name, hand, money):
+    def __init__(self, name="Unknown", money=0):
         self.name = name
-        self._hand = hand
         self.money = money
+        self._hand = Hand()
 
     def draw(self, cards):
         self._hand.add(cards)
 
     def discard(self, card):
         self.hand.remove(card)
+
+    def show_hand(self):
+        pass
 
 
     def __str__(self):
